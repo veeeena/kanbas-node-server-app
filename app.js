6285d8159a13b70a55b1132ef0c446ed1feb776b
Veena K <vkodakirthi@gmail.com>
2023-12-03 17:57:33 -0500

    a6.3

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ import Lab5 from "./Lab5.js"
 import cors from "cors";
 import CourseRoutes from "./courses/routes.js"
 import ModuleRoutes from "./modules/routes.js"
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+UserRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000);
